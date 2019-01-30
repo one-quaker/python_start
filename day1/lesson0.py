d06021416289c5375e707f75257261769e80636b
Anatoliy Kurinnyy <one.quaker@gmail.com>
2019-01-30 20:01:44 +0200

    update

--- day1/lesson0.py
+++ day1/lesson0.py
@@ -11,3 +11,10 @@ elif 'linux' in sys.platform:
     print('Linux rulez! pew pew')
 else:
     print('Unknown operating system')
+
+
+for i in range(10):
+    current_dir = os.getcwwd()
+    newdir_path = os.path.join(current_dir, 'test_{0}'.format(i))
+    print(current_dir, newdir_path)
+    # os.mkdir(newdir_path)
