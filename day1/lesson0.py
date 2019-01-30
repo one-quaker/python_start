fac8ddd1e514dda689bc7d5096b667673227a30f
Anatoliy Kurinnyy <one.quaker@gmail.com>
2019-01-30 19:54:53 +0200

    update

--- day1/lesson0.py
+++ day1/lesson0.py
@@ -1,4 +1,13 @@
 import os
+import sys
 
 
-print(os.listdir())
+# print(os.listdir())
+if sys.platform == 'darwin':
+    print('Hello Mac!')
+elif sys.platform == 'win32':
+    print('Dude, install linux or mac :)')
+elif 'linux' in sys.platform:
+    print('Linux rulez! pew pew')
+else:
+    print('Unknown operating system')
