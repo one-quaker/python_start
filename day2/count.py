005932df57aa88558c7b510a19794788cb4bb429
Anatoliy Kurinnyy <one.quaker@gmail.com>
2019-02-07 20:26:57 +0200

    update

--- day2/count.py
+++ day2/count.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(description='Countdown timer')
 parser.add_argument('-t', '--time', type=int, default=5, help='Time in seconds')
 parser.add_argument('-d', '--delay', type=int, default=1, help='Delay in seconds')
 parser.add_argument('-m', '--start-message', type=str, required=True, help='User message')
-parser.add_argument('-D', '--debug', action='store_true', default=False)
+parser.add_argument('-D', '--debug', action='store_true', default=False, help='Enable debug mode')
 
 
 ARG = parser.parse_args()
@@ -20,6 +20,7 @@ if ARG.debug:
 else:
     LOG_LEVEL = logging.INFO
 logging.basicConfig(format=LOG_TPL, level=LOG_LEVEL)
+log = logging.getLogger(__name__)
 
 
 # print(sys.argv)
@@ -31,11 +32,14 @@ t = ARG.time
 delay = ARG.delay
 
 
-while t > 0:
-    print('Live coding starts in {:.02f} minutes 👍💪'.format(t / 60))
+while t:
+    log.info('Live coding starts in {:.02f} minutes 👍💪'.format(t / 60))
     t -= delay
     time.sleep(delay)
+    log.debug('Delay: {}'.format(delay))
+    log.debug('Time: {}'.format(t))
 
 
-print(ARG.start_message)
-print(ARG.debug)
+log.warning(ARG.start_message)
+log.error('Test error')
+log.debug('Debug mode ON')
