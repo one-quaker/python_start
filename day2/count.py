0d1cc5ddc843b598b6ad0a8dbb3da1b745d28fe9
Anatoliy Kurinnyy <one.quaker@gmail.com>
2019-02-07 19:54:36 +0200

    update

--- day2/count.py
+++ day2/count.py
@@ -4,24 +4,24 @@ import sys
 import time
 
 
-# parser = argparse.ArgumentParser(description='Telegram bot')
-# parser.add_argument('--to-stdout', action='store_true', default=False)
+parser = argparse.ArgumentParser(description='Countdown timer')
+parser.add_argument('-t', '--time', type=int, default=5, help='Time in seconds')
 # parser.add_argument('--debug', action='store_true', default=False)
-# ARG = parser.parse_args()
+ARG = parser.parse_args()
 
 
-print(sys.argv)
-arg_name, arg_value = sys.argv[1].split('=')
-print(arg_name, arg_value)
+# print(sys.argv)
+# arg_name, arg_value = sys.argv[1].split('=')
+# print(arg_name, arg_value)
 
 
-t = 5 * 60
+t = ARG.time
 delay = 5
 
 
-# while t:
-#     print('Telegram bot, live coding starts in {:.02f} minutes 👍💪'.format(t / 60))
-#     t -= delay
-#     time.sleep(delay)
+while t:
+    print('Telegram bot, live coding starts in {:.02f} minutes 👍💪'.format(t / 60))
+    t -= delay
+    time.sleep(delay)
 
-# print('Start!!! 👍👍👍')
+print('Start!!! 👍👍👍')
