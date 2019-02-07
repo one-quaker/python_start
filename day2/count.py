cfd4702cc8cae4fbb65d311344199c72e63c0b36
Anatoliy Kurinnyy <one.quaker@gmail.com>
2019-02-07 20:12:53 +0200

    update

--- day2/count.py
+++ day2/count.py
@@ -6,6 +6,8 @@ import time
 
 parser = argparse.ArgumentParser(description='Countdown timer')
 parser.add_argument('-t', '--time', type=int, default=5, help='Time in seconds')
+parser.add_argument('-d', '--delay', type=int, default=1, help='Delay in seconds')
+parser.add_argument('-m', '--start-message', type=str, required=True, help='User message')
 # parser.add_argument('--debug', action='store_true', default=False)
 ARG = parser.parse_args()
 
@@ -16,12 +18,13 @@ ARG = parser.parse_args()
 
 
 t = ARG.time
-delay = 5
+delay = ARG.delay
 
 
-while t:
-    print('Telegram bot, live coding starts in {:.02f} minutes 👍💪'.format(t / 60))
+while t > 0:
+    print('Live coding starts in {:.02f} minutes 👍💪'.format(t / 60))
     t -= delay
     time.sleep(delay)
 
-print('Start!!! 👍👍👍')
+
+print(ARG.start_message)
