8e71480aacd05b394776c130358376f3ad88a898
Anatoliy Kurinnyy <one.quaker@gmail.com>
2019-03-13 15:13:45 +0200

    install docker fix

--- install_docker.sh
+++ install_docker.sh
@@ -8,12 +8,12 @@ UBUNTU_VERSION=`lsb_release -cs`
 # install
 # sh -c "$(wget https://raw.githubusercontent.com/one-quaker/python_start/master/install_docker.sh -O -)"
 
-sudo apt -y update
-sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+sudo apt update -y
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $UBUNTU_VERSION stable" | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt -y update
-sudo apt -y install docker-ce
+sudo apt update -y
+sudo apt install -y docker-ce
 sudo usermod -aG docker $USER
 curl -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` -o /tmp/docker-compose
 sudo mv -v /tmp/docker-compose /usr/local/bin/docker-compose
