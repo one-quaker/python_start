1013673bb80dd4fd14e696993991ec34b9da68e6
Anatoliy Kurinnyy <one.quaker@gmail.com>
2019-04-04 01:30:51 +0300

    admin panel update

--- day7/django_app/web/admin.py
+++ day7/django_app/web/admin.py
@@ -6,7 +6,7 @@ from .models import Post, Author
 class PostAdmin(admin.ModelAdmin):
     model = Post
 
-    list_display = ('title', 'author', 'tag_list', 'bookmark', 'view', 'rating', 'post_url', 'cover_img', 'created_at', 'description_short')
+    list_display = ('title', 'author', 'tag_list', 'bookmark', 'view', 'rating', 'post_url', 'cover_img', 'created_ts', 'description_short')
     list_filter = ('created_at', 'author')
     search_fields = ('tag_list', 'title', 'author__nickname', 'description_text')
     ordering = ['created_at', ]
@@ -15,13 +15,20 @@ class PostAdmin(admin.ModelAdmin):
         return mark_safe('<a href="{}" target="_blank">Open</a>'.format(obj.url))
 
     def cover_img(self, obj):
-        return mark_safe('<img src="{}" width="256" alt="No cover">'.format(obj.cover))
+        return mark_safe('<a href="{}" target="_blank"><img src="{}" width="256" alt="No cover"></a>'.format(obj.url.replace('#habracut', ''), obj.cover))
 
     def description_short(self, obj):
-        return '{} ...'.format(obj.description_text[:128])
+        word_count = 32
+        short_by_word = ' '.join(obj.description_text.split(' ')[0:word_count])
+        return '{} ...'.format(short_by_word)
+
+    def created_ts(self, obj):
+        return obj.created_at.strftime('%d/%b/%Y %H:%M:%S')
 
     def get_queryset(self, request):
         return super().get_queryset(request).select_related('author')
+
+    created_ts.admin_order_field = 'created_at'
 
 
 admin.site.register(Post, PostAdmin)
