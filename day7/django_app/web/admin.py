bc99c5a92a280723791731098baa16facf0859c5
Anatoliy Kurinnyy <one.quaker@gmail.com>
2019-04-03 22:39:19 +0300

    django_app admin update

--- day7/django_app/web/admin.py
+++ day7/django_app/web/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Post, Author
 
 
 class PostAdmin(admin.ModelAdmin):
     model = Post
 
-    list_display = ('title', 'author', 'tag_list', 'bookmark', 'view', 'rating', 'url', 'cover', 'created_at', 'description_text')
+    list_display = ('title', 'author', 'tag_list', 'bookmark', 'view', 'rating', 'post_url', 'cover_img', 'created_at', 'description_text')
     list_filter = ('created_at', 'author')
-    search_fields = ('tag_list', 'title')
+    search_fields = ('tag_list', 'title', 'author__nickname', 'description_text')
+    ordering = ['created_at', 'rating']
+
+    def post_url(self, obj):
+        return mark_safe('<a href="{}" target="_blank">Open</a>'.format(obj.url))
+
+    def cover_img(self, obj):
+        return mark_safe('<img src="{}" width="256" alt="No cover">'.format(obj.cover))
 
     def get_queryset(self, request):
         return super().get_queryset(request).select_related('author')
